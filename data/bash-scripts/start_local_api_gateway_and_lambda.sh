e7ff7c24d7a6ba8104a3a5b62e14d2a086ea41fb
Mipmipp <sebastianhernandoh@gmail.com>
2024-10-07 10:39:43 -0300

    Documented script

--- data/bash-scripts/start_local_api_gateway_and_lambda.sh
+++ data/bash-scripts/start_local_api_gateway_and_lambda.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
 
+# Base endpoint for the local AWS environment
 BASE_ENDPOINT=http://localhost:4566
-API_NAME=items_crud
-ROUTE_NAME=items
-STAGE=test
-REGION=us-east-1
-LAMBDA_ROLE=arn:aws:iam::123456789012:role/lambda-role
 
+# API configuration parameters
+API_NAME=items_crud              # Name of the API
+ROUTE_NAME=items                 # Route for the API
+STAGE=test                       # Deployment stage of the API
+REGION=us-east-1                 # AWS region for deployment
+LAMBDA_ROLE=arn:aws:iam::123456789012:role/lambda-role  # IAM role for Lambda functions
+
+# Function names for different CRUD operations
 GET_FUNCTION_NAME=test_items_get_function
 PUT_FUNCTION_NAME=test_items_put_function
 DELETE_FUNCTION_NAME=test_items_delete_function
 POST_FUNCTION_NAME=test_items_post_function
 
+# Function to handle failures and exit the script
 function fail() {
-    echo $2
-    exit $1
+    echo $2    # Print the error message
+    exit $1    # Exit the script with the specified error code
 }
 
+## https://docs.localstack.cloud/user-guide/aws/lambda/
+
+# Create the GET Lambda function
 aws --endpoint-url=${BASE_ENDPOINT} lambda create-function \
     --region ${REGION} \
     --function-name ${GET_FUNCTION_NAME} \
@@ -26,8 +34,10 @@ aws --endpoint-url=${BASE_ENDPOINT} lambda create-function \
     --zip-file fileb://C:/Users/CBA/localstack-ts-api-gateway-lambda-dynamodb-crud-example/dist/index.zip \
     --role ${LAMBDA_ROLE}
 
+# Check if the function creation was successful
 [ $? == 0 ] || fail 1 "Failed: AWS / lambda / create-function (GET)"
 
+# Create the PUT Lambda function
 aws --endpoint-url=${BASE_ENDPOINT} lambda create-function \
     --region ${REGION} \
     --function-name ${PUT_FUNCTION_NAME} \
@@ -37,8 +47,10 @@ aws --endpoint-url=${BASE_ENDPOINT} lambda create-function \
     --zip-file fileb://C:/Users/CBA/localstack-ts-api-gateway-lambda-dynamodb-crud-example/dist/index.zip \
     --role ${LAMBDA_ROLE}
 
+# Check if the function creation was successful
 [ $? == 0 ] || fail 1 "Failed: AWS / lambda / create-function (PUT)"
 
+# Create the DELETE Lambda function
 aws --endpoint-url=${BASE_ENDPOINT} lambda create-function \
     --region ${REGION} \
     --function-name ${DELETE_FUNCTION_NAME} \
@@ -48,8 +60,10 @@ aws --endpoint-url=${BASE_ENDPOINT} lambda create-function \
     --zip-file fileb://C:/Users/CBA/localstack-ts-api-gateway-lambda-dynamodb-crud-example/dist/index.zip \
     --role ${LAMBDA_ROLE}
 
+# Check if the function creation was successful
 [ $? == 0 ] || fail 1 "Failed: AWS / lambda / create-function (DELETE)"
 
+# Create the POST Lambda function
 aws --endpoint-url=${BASE_ENDPOINT} lambda create-function \
     --region ${REGION} \
     --function-name ${POST_FUNCTION_NAME} \
@@ -59,8 +73,10 @@ aws --endpoint-url=${BASE_ENDPOINT} lambda create-function \
     --zip-file fileb://C:/Users/CBA/localstack-ts-api-gateway-lambda-dynamodb-crud-example/dist/index.zip \
     --role ${LAMBDA_ROLE}
 
+# Check if the function creation was successful
 [ $? == 0 ] || fail 1 "Failed: AWS / lambda / create-function (POST)"
 
+# Retrieve the ARNs for the created Lambda functions
 LAMBDA_ARN_GET=$(aws --endpoint-url=${BASE_ENDPOINT} lambda list-functions \
     --query "Functions[?FunctionName==\`${GET_FUNCTION_NAME}\`].FunctionArn" --output text --region ${REGION})
 
@@ -73,29 +89,38 @@ LAMBDA_ARN_DELETE=$(aws --endpoint-url=${BASE_ENDPOINT} lambda list-functions \
 LAMBDA_ARN_POST=$(aws --endpoint-url=${BASE_ENDPOINT} lambda list-functions \
     --query "Functions[?FunctionName==\`${POST_FUNCTION_NAME}\`].FunctionArn" --output text --region ${REGION})
 
+## https://docs.localstack.cloud/user-guide/aws/apigateway/
+
+# Create the API Gateway REST API
 aws --endpoint-url=${BASE_ENDPOINT} apigateway create-rest-api \
     --region ${REGION} \
     --name ${API_NAME}
 
+# Check if the API creation was successful
 [ $? == 0 ] || fail 2 "Failed: AWS / apigateway / create-rest-api"
 
+# Retrieve the API ID for the created REST API
 API_ID=$(aws --endpoint-url=${BASE_ENDPOINT} apigateway get-rest-apis \
     --query "items[?name==\`${API_NAME}\`].id" --output text --region ${REGION})
 
+# Get the parent resource ID (the root resource)
 PARENT_RESOURCE_ID=$(aws --endpoint-url=${BASE_ENDPOINT} apigateway get-resources \
     --rest-api-id ${API_ID} \
     --query 'items[?path==`/`].id' --output text --region ${REGION})
 
+# Create a new resource under the root resource ("/items")
 aws --endpoint-url=${BASE_ENDPOINT} apigateway create-resource \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
     --parent-id ${PARENT_RESOURCE_ID} \
     --path-part "items"
 
+# Retrieve the resource ID for the newly created "/items" resource
 RESOURCE_ID_ALL=$(aws --endpoint-url=${BASE_ENDPOINT} apigateway get-resources \
     --rest-api-id ${API_ID} \
     --query 'items[?path==`/items`].id' --output text --region ${REGION})
 
+# Define the GET method for the "/items" resource
 aws --endpoint-url=${BASE_ENDPOINT} apigateway put-method \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
@@ -103,6 +128,7 @@ aws --endpoint-url=${BASE_ENDPOINT} apigateway put-method \
     --http-method GET \
     --authorization-type "NONE"
 
+# Define the integration for the GET method
 aws --endpoint-url=${BASE_ENDPOINT} apigateway put-integration \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
@@ -113,18 +139,22 @@ aws --endpoint-url=${BASE_ENDPOINT} apigateway put-integration \
     --uri arn:aws:apigateway:${REGION}:lambda:path/2015-03-31/functions/${LAMBDA_ARN_GET}/invocations \
     --passthrough-behavior WHEN_NO_MATCH
 
+# Check if the integration was successful
 [ $? == 0 ] || fail 5 "Failed: AWS / apigateway / put-integration (GET ALL)"
 
+# Create a resource for individual items ("/items/{itemId}")
 aws --endpoint-url=${BASE_ENDPOINT} apigateway create-resource \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
     --parent-id ${RESOURCE_ID_ALL} \
     --path-part "{itemId}"
 
+# Retrieve the resource ID for the "/items/{itemId}" resource
 RESOURCE_ID=$(aws --endpoint-url=${BASE_ENDPOINT} apigateway get-resources \
     --rest-api-id ${API_ID} \
     --query 'items[?path==`/items/{itemId}`].id' --output text --region ${REGION})
 
+# Define the GET method for the "/items/{itemId}" resource
 aws --endpoint-url=${BASE_ENDPOINT} apigateway put-method \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
@@ -133,8 +163,10 @@ aws --endpoint-url=${BASE_ENDPOINT} apigateway put-method \
     --request-parameters "method.request.path.itemId=true" \
     --authorization-type "NONE"
 
+# Check if the GET method creation was successful
 [ $? == 0 ] || fail 4 "Failed: AWS / apigateway / put-method (GET ITEM)"
 
+# Define the integration for the GET method of an individual item
 aws --endpoint-url=${BASE_ENDPOINT} apigateway put-integration \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
@@ -145,8 +177,10 @@ aws --endpoint-url=${BASE_ENDPOINT} apigateway put-integration \
     --uri arn:aws:apigateway:${REGION}:lambda:path/2015-03-31/functions/${LAMBDA_ARN_GET}/invocations \
     --passthrough-behavior WHEN_NO_MATCH
 
+# Check if the integration for the GET method was successful
 [ $? == 0 ] || fail 5 "Failed: AWS / apigateway / put-integration (GET ITEM)"
 
+# Define the PUT method for the "/items/{itemId}" resource
 aws --endpoint-url=${BASE_ENDPOINT} apigateway put-method \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
@@ -155,8 +189,10 @@ aws --endpoint-url=${BASE_ENDPOINT} apigateway put-method \
     --request-parameters "method.request.path.itemId=true" \
     --authorization-type "NONE"
 
+# Check if the PUT method creation was successful
 [ $? == 0 ] || fail 4 "Failed: AWS / apigateway / put-method (PUT ITEM)"
 
+# Define the integration for the PUT method
 aws --endpoint-url=${BASE_ENDPOINT} apigateway put-integration \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
@@ -167,8 +203,10 @@ aws --endpoint-url=${BASE_ENDPOINT} apigateway put-integration \
     --uri arn:aws:apigateway:${REGION}:lambda:path/2015-03-31/functions/${LAMBDA_ARN_PUT}/invocations \
     --passthrough-behavior WHEN_NO_MATCH
 
+# Check if the integration for the PUT method was successful
 [ $? == 0 ] || fail 5 "Failed: AWS / apigateway / put-integration (PUT ITEM)"
 
+# Define the DELETE method for the "/items/{itemId}" resource
 aws --endpoint-url=${BASE_ENDPOINT} apigateway put-method \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
@@ -177,8 +215,10 @@ aws --endpoint-url=${BASE_ENDPOINT} apigateway put-method \
     --request-parameters "method.request.path.itemId=true" \
     --authorization-type "NONE"
 
+# Check if the DELETE method creation was successful
 [ $? == 0 ] || fail 4 "Failed: AWS / apigateway / put-method (DELETE ITEM)"
 
+# Define the integration for the DELETE method
 aws --endpoint-url=${BASE_ENDPOINT} apigateway put-integration \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
@@ -189,8 +229,10 @@ aws --endpoint-url=${BASE_ENDPOINT} apigateway put-integration \
     --uri arn:aws:apigateway:${REGION}:lambda:path/2015-03-31/functions/${LAMBDA_ARN_DELETE}/invocations \
     --passthrough-behavior WHEN_NO_MATCH
 
+# Check if the integration for the DELETE method was successful
 [ $? == 0 ] || fail 5 "Failed: AWS / apigateway / put-integration (DELETE ITEM)"
 
+# Define the POST method for the "/items" resource
 aws --endpoint-url=${BASE_ENDPOINT} apigateway put-method \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
@@ -198,8 +240,10 @@ aws --endpoint-url=${BASE_ENDPOINT} apigateway put-method \
     --http-method POST \
     --authorization-type "NONE"
 
+# Check if the POST method creation was successful
 [ $? == 0 ] || fail 4 "Failed: AWS / apigateway / put-method (POST ITEMS)"
 
+# Define the integration for the POST method
 aws --endpoint-url=${BASE_ENDPOINT} apigateway put-integration \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
@@ -210,17 +254,23 @@ aws --endpoint-url=${BASE_ENDPOINT} apigateway put-integration \
     --uri arn:aws:apigateway:${REGION}:lambda:path/2015-03-31/functions/${LAMBDA_ARN_POST}/invocations \
     --passthrough-behavior WHEN_NO_MATCH
 
+# Check if the integration for the POST method was successful
 [ $? == 0 ] || fail 5 "Failed: AWS / apigateway / put-integration (POST ITEMS)"
 
+# Create a deployment for the API
 aws --endpoint-url=${BASE_ENDPOINT} apigateway create-deployment \
     --region ${REGION} \
     --rest-api-id ${API_ID} \
     --stage-name ${STAGE}
 
+# Check if the deployment was successful
 [ $? == 0 ] || fail 6 "Failed: AWS / apigateway / create-deployment"
 
+# Define the endpoint for accessing the API
 ENDPOINT=${BASE_ENDPOINT}/restapis/${API_ID}/${STAGE}/_user_request_/items
 
-echo "API_ID=${API_ID}" >> .local.env
+# Save the endpoint to a local environment file
+echo "LOCAL_API_ENDPOINT=${ENDPOINT}" >> .local.env
 
+# Output the API endpoint
 echo "API available at: ${ENDPOINT}"
